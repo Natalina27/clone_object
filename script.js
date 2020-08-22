32e870c161ba5f0a570172a14455b093f7deba7d
Natalya Myunster <natalyamyunster@yahoo.com>
2020-08-22 07:46:40 +0200

    clone function and date format

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function clone(obj) {
     // Handle Array
     if (obj instanceof Array) {
         copy = [];
-        for (let i = 0, len = obj.length; i < len; i++) {
+        for (let i = 0; i < obj.length; i++) {
             copy[i] = clone(obj[i]);
         }
         return copy;
@@ -40,6 +40,8 @@ const objNum = 8;
 const objStr = 'string';
 const objBool = true;
 
+
+
 console.log(clone(objNull)); // null
 console.log(clone(objUndef)); //undefined
 console.log(clone(objNum)); // 8
@@ -47,3 +49,22 @@ console.log(clone(objStr)); // string
 console.log(clone(objBool)); // true
 
 // Handle Date
+const objDate = new Date();
+console.log(clone(objDate)); //Sat Aug 22 2020 07:41:26 GMT+0200 (Central European Summer Time)
+
+//date format
+let today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+const yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+console.log('today ', today);
+
+// Handle Array
+const objArray = [1, 2, 3, 4, 5];
+console.log(clone(objArray)); //
+
+// Handle Object
+const objObj = {a: 1, b: 2};
+console.log(clone(objObj)); //
